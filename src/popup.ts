154dde34307fa05c792ef5bd5714823c61d41889
mtsuts <mariamtsuts@gmail.com>
2025-01-19 23:45:02 -0800

    popup-design-updates

--- src/popup.ts
+++ src/popup.ts
@@ -3,16 +3,16 @@ export function drawPopup(svg: any, data: any) {
   svg.selectAll('.popup-object').remove()
   const foreignObject = svg
     .append('foreignObject')
-    .attr('x', 700)
+    .attr('x', 500)
     .attr('y', 150)
-    .attr('width', '40%')
-    .attr('height', '40%')
+    .attr('width', '60%')
+    .attr('height', '100%')
     .attr('class', 'popup-object')
     .style('overflow', 'visible')
+    .style('background-color', '')
     .style('background-image', "url('./popup-background.png')")
     .style('background-size', '100% 100%')
     .style('background-repeat', 'no-repeat')
-    // .style('padding', '20px')
 
   foreignObject
     .append('xhtml:div')
@@ -25,11 +25,55 @@ export function drawPopup(svg: any, data: any) {
     .style('align-items', 'center')
     .style('justify-content', 'center')
     .style('width', '100%')
-    .style('height', '100px').html(`
-      <div> 
-      <div class='popup-container'> 
-      <img src='./popup-play.png' alt='play'/>
-      <div> ${data.Market} </div>
+    .style('overflow', 'auto')  
+    .style('height', '600px').html(`
+      <div class='popup'> 
+
+      <div class='popup-header'> 
+      <img src='./popup-play.svg' width=37 height=42 alt='play'/>
+      <img src='./popup-title-left.svg' />
+      <div class='popup-title'> ${data.Market} </div>
+         <img src='./popup-title-right.svg' />
       </div> 
+
+      <div class='popup-bullets'> 
+         <div class='bullet'>
+         <div class='bullet-title'> ${
+           data['% Homes Passed (pct availability)'] || ''
+         } </div>
+       <div> Homes <br/> Passed % </div>
+          </div> 
+
+           <div class='bullet'>
+         <div class='bullet-title'> ${data['Market Share %'] || ''} </div>
+          <div> Market <br/> share % </div>
+          </div> 
+
+           <div class='bullet'>
+         <div class='bullet-title'> ${
+           data['Multi-Cultural (Hispanic, Asian, African American)'] || ''
+         } </div>
+        <div> Multicultural % </div>
+          </div> 
+
+           <div class='bullet'>
+         <div class='bullet-title'> ${data['Gateway Index'] || ''} </div>
+         <div> Homes <br/> Passed % </div>
+          </div> 
+      </div>
+      <img src='./bullet-bottom-line.svg' width='500px' alt='line'/>
+
+      <div class='pie-chart-section'> 
+      <img src='./left-line.svg' height='200px' alt='left-line'/>
+      <div> pie chart  </div>
+       <div class='pie-bullets'>
+       <div class='pie-bullets-group'> 
+        <img src='./bullets/rethink.svg' alt='rething-rect'/>
+        <div> Rethink </div>
+       </div> 
+       </div>
+        <img src='./right-line.svg' height='200px' alt='right-line'/>
+      </div>
+
       </div>`)
 }
