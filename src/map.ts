248b35ca9f9014bdec681f78302906fa52f08fa0
mtsuts <mariamtsuts@gmail.com>
2025-01-21 22:58:24 +0400

    zoom-tranform-updates

--- src/map.ts
+++ src/map.ts
@@ -221,8 +221,8 @@ export function drawMap(params: any) {
       2,
       2 / Math.max((x1 - x0) / width, (y1 - y0) / height)
     )
-    const translateX = width / 2 - (scale * (x0 + x1)) / 2
-    const translateY = height / 2 - (scale * (y0 + y1)) / 2
+    const translateX = width / 2 - (scale * (x0 + x1)) / 1.7
+    const translateY = height / 2 - (scale * (y0 + y1)) / 1.7
 
     svg
       .transition()
